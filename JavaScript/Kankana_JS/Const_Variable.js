bcfaa291d617beeacb645aa31c68c6987efd4aba
Kankana Memane <kankanalearning@gmail.com>
2026-04-16 17:13:33 +0530

    Updated const variable comments

--- JavaScript/Kankana_JS/Const_Variable.js
+++ JavaScript/Kankana_JS/Const_Variable.js
@@ -1,2 +1,11 @@
-const mobile=9011336600;
-console.log("Constant variable",mobile);
+
+// Demonstrates `const` in JavaScript. A `const` variable cannot be reassigned after it's created.
+
+const mobile = 9011336600;
+console.log("Constant variable", mobile);
+
+// The reassignment below is commented out because it would throw. a TypeError at runtime: "Assignment to constant variable."
+/*
+mobile = 980897777777; // Uncaught TypeError: Assignment to constant variable.
+console.log("Updated Constant variable", mobile);
+*/
